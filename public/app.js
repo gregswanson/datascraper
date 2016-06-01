a564a087fb47cecbd270b2e30c99b8b35c8009e2
Gregory Swanson <gswanson@gregorys-mbp.fios-router.home>
2016-06-01 01:51:25 -0400

    css added

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ var counter = 0;
 $.getJSON('/articles', function(data) {
 	
  
-  	$('#articles').append('<article data-id="' + data[0]._id + '"><a href="https://news.vice.com/' + data[0].link +'"<h3>'+ data[0].title + '</h3></a><br/><p>'+ data[0].body + '</p></article>');
+  	$('#articles').append('<article data-id="' + data[0]._id + '"><h3 class="bodytitle">'+ data[0].title + '</h3><br/><p class="bodytxt">'+ data[0].body + '</p><br><a href="https://news.vice.com/' + data[0].link +'" class="bodyread">Read Full Article <i class="glyphicon glyphicon-share-alt"></i></a></article>');
   	//console.log(counter);
    // if(data.note){
      
@@ -16,12 +16,12 @@ $.getJSON('/articles', function(data) {
   		$('#articles').text('');
   		if( counter >= 0) {
   			
-  			$('#articles').append('<article data-id="' + data[counter]._id + '"><a href="https://news.vice.com/' + data[counter].link +'"<h3>'+ data[counter].title + '</h3></a><br/><p>'+ data[counter].body + '</p></article>');
+  			$('#articles').append('<article data-id="' + data[counter]._id + '"><h3 class="bodytitle">'+ data[counter].title + '</h3><br/><p class="bodytxt">'+ data[counter].body + '</p><br><a href="https://news.vice.com/' + data[counter].link +'" class="bodyread">Read Full Article <i class="glyphicon glyphicon-share-alt"></i></a></article>');
   			//console.log(counter);
   			return;
   		} else {
   			counter = data.length-1;
-  			$('#articles').append('<article data-id="' + data[data.length-1]._id + '"><a href="https://news.vice.com/' + data[data.length-1].link +'"<h3>'+ data[data.length-1].title + '</h3></a><br/><p>'+ data[data.length-1].body + '</p></article>');
+  			$('#articles').append('<article data-id="' + data[data.length-1]._id + '"><h3 class="bodytitle">'+ data[data.length-1].title + '</h3><br/><p class="bodytxt">'+ data[data.length-1].body + '</p><br><a href="https://news.vice.com/' + data[data.length-1].link +'" class="bodyread">Read Full Article <i class="glyphicon glyphicon-share-alt"></i></a></article>');
 	  		//console.log(counter);
 	  		return;
   		}
@@ -33,12 +33,12 @@ $.getJSON('/articles', function(data) {
   		$('#articles').text('');
   		if( counter <= data.length-1) {
   			
-  			$('#articles').append('<article data-id="' + data[counter]._id + '"><a href="https://news.vice.com/' + data[counter].link +'"<h3>'+ data[counter].title + '</h3></a><br/><p>'+ data[counter].body + '</p></article>');
+  			$('#articles').append('<article data-id="' + data[counter]._id + '"><h3 class="bodytitle">'+ data[counter].title + '</h3><br/><p class="bodytxt">'+ data[counter].body + '</p><br><a href="https://news.vice.com/' + data[counter].link +'" class="bodyread">Read Full Article <i class="glyphicon glyphicon-share-alt"></i></a></article>');
   			//console.log(counter);
   			return;
   		} else {
   			counter = 0;
-  			$('#articles').append('<article data-id="' + data[0]._id + '"><a href="https://news.vice.com/' + data[0].link +'"<h3>'+ data[0].title + '</h3></a><br/><p>'+ data[0].body + '</p></article>');
+  			$('#articles').append('<article data-id="' + data[0]._id + '"><h3 class="bodytitle">'+ data[0].title + '</h3><br/><p class="bodytxt">'+ data[0].body + '</p><br><a href="https://news.vice.com/' + data[0].link +'" class="bodyread">Read Full Article <i class="glyphicon glyphicon-share-alt"></i></a></article>');
 	  		//console.log(counter);
 	  		return;
   		}
@@ -54,11 +54,11 @@ $.ajax({
       //console.log(data);
 
       if(data.note){
-        $('#notedisplay').append('<h3 data-id="'+ data.note._id + '">'+ data.note.title + '</h3');
-        $('#notedisplay').append('<p>' + data.note.body + '</p>');
+        $('#notedisplay').append('<h3 class="notetxttitle" data-id="'+ data.note._id + '">'+ data.note.title + '</h3');
+        $('#notedisplay').append('<p class="notetxt">' + data.note.body + '</p>');
       } else {
-        $('#notedisplay').append('<h3> There are no notes to display </h3');
-        $('#notedisplay').append('<p> Please add a note </p>');
+        $('#notedisplay').append('<h3 class="notetxt"> There are no notes to display </h3');
+        $('#notedisplay').append('<p class="notetxt"> Please add a note </p>');
       }
 
     });
@@ -86,11 +86,11 @@ $(document).on('click', 'button', function(){
       //console.log(data);
 
       if(data.note){
-        $('#notedisplay').append('<h3 data-id="'+ data.note._id + '">'+ data.note.title + '</h3');
-        $('#notedisplay').append('<p>' + data.note.body + '</p>');
+        $('#notedisplay').append('<h3 class="notetxttitle" data-id="'+ data.note._id + '">'+ data.note.title + '</h3');
+        $('#notedisplay').append('<p class="notetxt">' + data.note.body + '</p>');
       } else {
-      	$('#notedisplay').append('<h3> There are no notes to display </h3');
-        $('#notedisplay').append('<p> Please add a note </p>');
+      	$('#notedisplay').append('<h3 class="notetxt"> There are no notes to display </h3');
+        $('#notedisplay').append('<p class="notetxt"> Please add a note </p>');
       }
 
     });
@@ -121,10 +121,8 @@ $('#addnote').on("click", function(){
 	$.post( URL, data)
 		.done(function(data){
 			//console.log(data);
-		})
-
-	$('#noteinputtitle').val('');
-	$('#noteinput').val('');
+$('#noteinputtitle').val('');
+  $('#noteinput').val('');
 
   $.ajax({
     method: "GET",
@@ -136,16 +134,14 @@ $('#addnote').on("click", function(){
       $('#notedisplay').empty();
 
       if(data.note){
-        $('#notedisplay').append('<h3 data-id="'+ data.note._id + '">'+ data.note.title + '</h3');
-        $('#notedisplay').append('<p>' + data.note.body + '</p>');
+        $('#notedisplay').append('<h3 class="notetxttitle" data-id="'+ data.note._id + '">'+ data.note.title + '</h3');
+        $('#notedisplay').append('<p class="notetxt">' + data.note.body + '</p>');
       } else {
-        $('#notedisplay').append('<h3> There are no notes to display </h3');
-        $('#notedisplay').append('<p> Please add a note </p>');
+        $('#notedisplay').append('<h3 class="notetxt"> There are no notes to display </h3');
+        $('#notedisplay').append('<p class="notetxt"> Please add a note </p>');
       }
-
+		})
     });
-
-
 
 	return false;
 
@@ -157,7 +153,7 @@ $('#addnote').on("click", function(){
 $('#removenote').on('click', function(){
 
   var thisId = $('#notedisplay').children().attr('data-id');
-  alert(thisId);
+  //alert(thisId);
 
   var URL = "/remove/" + thisId;
 
@@ -165,24 +161,5 @@ $('#removenote').on('click', function(){
     .done(function(data){
       //console.log(data.note);
     });
-
-
-  //   $.ajax({
-  //   method: "GET",
-  //   url: "/articles/" + thisId,
-  // })
-  //   .done(function( data ) {
-  //     console.log(data);
-
-  //     if(data.note){
-  //       $('#notedisplay').append('<h3 data-id="'+ data.note._id + '">'+ data.note.title + '</h3');
-  //       $('#notedisplay').append('<p>' + data.note.body + '</p>');
-  //     } else {
-  //       $('#notedisplay').append('<h3> There are no notes to display </h3');
-  //       $('#notedisplay').append('<p> Please add a note </p>');
-  //     }
-
-  //   });
-
 
 })
